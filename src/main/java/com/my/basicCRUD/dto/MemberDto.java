3dc744a7706b0e7fe4315671a42e07a1c9b5384a
lminjiii <lminjiiiii@gmail.com>
2025-02-18 10:43:44 +0900

    #1. initialize - DB 연결 - 환경변수 셋팅 - 폼 작업 - DTO -> Entity 변환

---
+++ src/main/java/com/my/basicCRUD/dto/MemberDto.java
@@ -0,0 +1,43 @@
+package com.my.basicCRUD.dto;
+
+import com.my.basicCRUD.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberDto {
+    private Long id;
+    private String name;
+    private int age;
+    private String address;
+
+//    public MemberDto(Long id, String name, int age, String address) {
+//        this.id = id;
+//        this.name = name;
+//        this.age = age;
+//        this.address = address;
+//    }
+//
+    // Entity -> DTO 변환
+    public static MemberDto fromEntity(Member entity) {
+        return new MemberDto(
+                entity.getMemberId(),
+                entity.getName(),
+                entity.getAge(),
+                entity.getAddress()
+        );
+    }
+
+    // DTO -> Entity 변환
+    public static Member fromDto(MemberDto dto) {
+        Member member = new Member();
+        member.setMemberId(dto.getId());
+        member.setName(dto.getName());
+        member.setAge(dto.getAge());
+        member.setAddress(dto.getAddress());
+        return member;
+    }
+}
